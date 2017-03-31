271d51497f604403a4bcf8cdf8b18a2365bdb92e
Keith Alpichi <keithalpichi@gmail.com>
2017-03-30 19:42:06 -0700

    edit renderColumns function to take into consideration for offset indexes from result

--- client/src/components/TrendingVideos.jsx
+++ client/src/components/TrendingVideos.jsx
@@ -5,20 +5,27 @@ import { connect } from 'react-redux';
 
 const renderColumns = (array, columnAmount) => {
   let map = []
+  let leftOverIndex = null
   for (var i = 0; i < array.length; i += columnAmount) {
     let innerArray = []
     for (var j = i; j < columnAmount; j++) {
       let innerItem = array[j]
       innerArray.push(innerItem)
     }
-    map.push(innerArray)
+    if (i !== j) { map.push(innerArray) }
+    if (i + columnAmount >= array.length) {
+      leftOverIndex = i
+    }
+  }
+  if (leftOverIndex) {
+    map.push(array.slice(leftOverIndex))
   }
   return <Grid container padded columns={columnAmount}>
-    {map.map(col => {
-      return <Grid.Row>
-        {col.map((pitch, i) => (
-          <Grid.Column>
-            <TrendingVideoCard pitch={pitch} key={i} />
+    {map.map((col, i) => {
+      return <Grid.Row key={i}>
+        {col.map((pitch, j) => (
+          <Grid.Column key={j}>
+            <TrendingVideoCard pitch={pitch} />
           </Grid.Column>
       ))}
     </Grid.Row>
