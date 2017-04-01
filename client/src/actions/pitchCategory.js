47e971c5604c3ce6c17222da84c70f278c49dc24
Keith Alpichi <keithalpichi@gmail.com>
2017-03-31 21:33:06 -0700

    add pitchCategory to state and its reducer. Is used on TrendingVideos pitch category tab to render the appropriate tab

---
+++ client/src/actions/pitchCategory.js
@@ -0,0 +1,11 @@
+export const setPitchCategoryToTop = () => {
+  return {
+    type: 'TRENDING_PITCH'
+  }
+}
+
+export const setPitchCategoryToTrending = () => {
+  return {
+    type: 'TOP_PITCH'
+  }
+}
