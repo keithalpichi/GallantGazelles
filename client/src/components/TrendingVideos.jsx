8cbd9a2fadad3f44561e0dcbcc24a4d5e8693c47
Keith Alpichi <keithalpichi@gmail.com>
2017-03-31 21:35:20 -0700

    replace functional stateless function with class in order to use map(*)ToProps functions. Also, edit the render function

--- client/src/components/TrendingVideos.jsx
+++ client/src/components/TrendingVideos.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TrendingVideoCard from './TrendingVideoCard.jsx'
-import { Card, Divider, Grid, Header, Icon, Image } from 'semantic-ui-react';
+import { Card, Divider, Grid, Header, Icon, Image, Menu, Segment } from 'semantic-ui-react';
+import { setPitchCategoryToTop, setPitchCategoryToTrending } from '../actions/pitchCategory'
 import { connect } from 'react-redux';
 
 const renderColumns = (array, columnAmount) => {
@@ -33,31 +34,72 @@ const renderColumns = (array, columnAmount) => {
   </Grid>
 }
 
-const TrendingVideos = (props) => {
-  if (props.pitches.length > 0) {
+// const TrendingVideos = (props) => {
+//   console.log('TrendingVideos props ', props);
+//   if (props.pitches.length > 0) {
+//     return (
+//       <section>
+//         <Divider horizontal>
+//           <Header as='h4'>
+//             <Icon name='line chart' />
+//             Trending Pitches
+//           </Header>
+//         </Divider>
+//         <Divider hidden />
+//         <Menu attached='top'>
+//           <Menu.Item name='trending' active={props.pitchCategory === 'trending'} onClick={this.props.setPitchCategoryToTrending} />
+//           <Menu.Item name='top' active={props.pitchCategory === 'top'} onClick={this.props.setPitchCategoryToTop} />
+//         </Menu>
+//         <Segment attached='bottom'>
+//           {renderColumns(props.pitches, 3)}
+//         </Segment>
+//       </section>
+//     )
+//   } else {
+//     return null
+//   }
+// }
+
+class TrendingVideos extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
     return (
       <section>
         <Divider horizontal>
           <Header as='h4'>
             <Icon name='line chart' />
-            Trending Pitches
+            {this.props.pitchCategory[0].toUpperCase() + this.props.pitchCategory.slice(1)} Pitches
           </Header>
         </Divider>
         <Divider hidden />
-        {renderColumns(props.pitches, 3)}
+        <Menu attached='top'>
+          <Menu.Item name='trending' active={this.props.pitchCategory === 'trending'} onClick={this.props.setPitchCategoryToTrending} />
+          <Menu.Item name='top' active={this.props.pitchCategory === 'top'} onClick={this.props.setPitchCategoryToTop} />
+        </Menu>
+        <Segment attached='bottom'>
+          {this.props.pitches.length > 0 ? renderColumns(this.props.pitches, 3) : <div>There is currently no {this.props.pitchCategory} pitches.</div>}
+        </Segment>
       </section>
-    )
-  } else {
-    return (
-      <div>NOPE!</div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
+  console.log('TrendingVideos.js ', state.pitches);
   return {
-    pitches: state.pitches.pitches
+    pitches: state.pitchCategory === 'top' ? state.pitches.topPitches : state.pitches.trendingPitches,
+    pitchCategory: state.pitchCategory
   }
 }
 
-export default connect(mapStateToProps)(TrendingVideos)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setPitchCategoryToTop: () => dispatch(setPitchCategoryToTop()),
+    setPitchCategoryToTrending: () => dispatch(setPitchCategoryToTrending())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrendingVideos)
