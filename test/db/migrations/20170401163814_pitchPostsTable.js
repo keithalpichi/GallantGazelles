acee48665343d37d69b9618e9e9548ae44fe2477
Keith Alpichi <keithalpichi@gmail.com>
2017-04-01 17:21:44 -0700

    add migration for pitch_posts n pitch_comments table

--- test/db/migrations/20170401163814_pitchPostsTable.js
+++ test/db/migrations/20170401163814_pitchPostsTable.js
@@ -1,14 +1,26 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('pitchPosts', (table) => {
+  return knex.schema.createTable('pitch_posts', (table) => {
     table.increments()
     table.string('title').notNullable();
     table.string('description').notNullable();
     table.integer('user_id').notNullable();
     table.integer('pitch_id').notNullable();
     table.timestamp('timestamp').defaultTo(knex.fn.now());
-  });
+  })
+  .then(() => {
+    return knex.schema.createTable('pitch_comments', (table) => {
+  		table.increments();
+  		table.string('comment').notNullable();
+  		table.integer('user_id').notNullable();
+  		table.integer('post_id').notNullable();
+  		table.timestamp('timestamp').defaultTo(knex.fn.now());
+  	});
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('pitchPosts')
+  return knex.schema.dropTable('pitch_posts')
+  .then(() => {
+    return knex.schema.dropTable('pitch_comments')
+  })
 };
